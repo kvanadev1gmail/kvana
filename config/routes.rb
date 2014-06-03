e11f359549ca10d9ebb3eb4c8f0940a3e5a76aa3
kvana <kvanadev1@gmail.com>
2014-06-03 19:17:29 +0530

    some ui changed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
 
 
-
   get 'kvana/ourprojects'
 
   get 'kvana/privacyandterms'
@@ -16,11 +15,11 @@ Rails.application.routes.draw do
 
   root  'kvana#home'
   match '/home', to:'kvana#home',     via:'get'
-  match '/help',    to: 'kvana#help',    via: 'get'
-  match '/about',   to: 'kvana#about',   via: 'get'
-  match '/contact', to: 'kvana#contactUs', via: 'get'
-  match '/ourprojects', to: 'kvana#ourprojects', via: 'get'
-  match '/privacyandterms', to: 'kvana#privacyandterms', via: 'get'
+  match '/help',    to:'kvana#help',    via:'get'
+  match '/about',   to:'kvana#about',   via:'get'
+  match '/contact', to:'kvana#contactUs', via:'get'
+  match '/ourprojects', to:'kvana#ourprojects', via:'get'
+  match '/privacyandterms', to:'kvana#privacyandterms', via:'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
